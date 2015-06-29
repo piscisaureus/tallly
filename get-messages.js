c6981a95685ef98fb574a1abd8c17afd45cc35bb
Bert Belder <bertbelder@gmail.com>
2015-06-29 11:37:02 -0700

    mile high club

--- get-messages.js
+++ get-messages.js
@@ -1,37 +1,75 @@
 
-module.exports = getMessages;
+module.exports = IssueCommentStream;
 
 
-function getMessages(github, issue, callback) {
-  var page = 1;
-  var perPage = 100;
+function IssueCommentStream(client, issue) {
+  this._client = client;
+  this._issue = issue;
 
-  var messages = [issue];
-
-  getPage();
-
-  function getPage() {
-    var options = {
-      user: issue.repository.owner.login,
-      repo: issue.repository.name,
-      number: issue.number,
-      page: page,
-      perPage: perPage
-    };
-    github.issues.getComments(options, onPage);
-  }
-
-  function onPage(err, result) {
-    if (err)
-      return callback(err);
-
-    messages = messages.concat(result);
-
-    if (result.length < perPage)
-      return callback(null, messages);
-
-    // Fetch more comments
-    page++;
-    process.nextTick(getPage);
-  }
+  this._queue = [];
+  this._fetching = false;
+  this._ended = false;
+  this._page = 1;
+  this._perPage = 64;
+  this._retryDelay = 60 * 1000;
 }
+
+
+IssueCommentStream.prototype._read = function() {
+  var next = this._queue.shift();
+  if (next)
+    return this.push(next);
+
+  if (this._ended)
+    return this.push(null);
+
+  if (this._fetching)
+    return;
+
+  this._fetch();
+};
+
+
+IssueCommentStream.prototype._fetch = function() {
+  assert(!this._fetching);
+  this._fetching = true;
+
+  var issue = this._issue;
+  var repo = issue.repository;
+
+  var query = {
+    user: repo.owner.login,
+    repo: repo.name,
+    number: issue.number,
+    page: this._page,
+    per_page: this._perPage,
+    sort: 'created',
+    direction: 'asc'
+  };
+
+  this._client.issues.getComments(query, this._onComments.bind(this));
+};
+
+
+IssueCommentStream.prototype._onComments = function(err, comments) {
+  if (err)
+    return setTimeout(this._fetch.bind(this), this._retryDelay);
+
+  assert(this._fetching);
+  this._fetching = false;
+
+  assert(!this._queue.length);
+  assert(!this._ended);
+  this._queue = comments;
+
+  if (comments.length < this._perPage)
+    this._ended = true;
+  else
+    this._page++;
+
+  var next = comments.shift() || null;
+  this.push(next);
+};
+
+
+
